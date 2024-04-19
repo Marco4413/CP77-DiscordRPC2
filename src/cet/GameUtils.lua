8231ffd4f9f0f7b0a3130b744ff3c5e678249c51
Marco <46133335+Marco4413@users.noreply.github.com>
2024-04-19 14:41:36 +0200

    CName.new does not exist on startup

--- src/cet/GameUtils.lua
+++ src/cet/GameUtils.lua
@@ -74,9 +74,6 @@ function GameUtils.GetVehicleName(vehicle)
     return Game.GetLocalizedTextByKey(vehicleRecord:DisplayName())
 end
 
-local _CNAME_VEHICLE_UI = CName.new("vehicle_ui")
-local _CNAME_SPEED_TO_MULTIPLIER = CName.new("speed_to_multiplier")
-
 ---@param vehicle gamevehicleObject
 ---@return number|nil vehicleSpeed The speed of the vehicle in mph as shown in the HUD
 ---@return boolean goingForward
@@ -89,6 +86,9 @@ function GameUtils.GetVehicleSpeed(vehicle)
     -- speedValue = AbsF(speedValue);
     -- let multiplier: Float = GameInstance.GetStatsDataSystem(this.m_activeVehicle.GetGame()).GetValueFromCurve(n"vehicle_ui", speedValue, n"speed_to_multiplier");
     -- inkTextRef.SetText(this.m_SpeedValue, IntToString(RoundMath(speedValue * multiplier)));
+
+    local _CNAME_VEHICLE_UI = CName.new("vehicle_ui")
+    local _CNAME_SPEED_TO_MULTIPLIER = CName.new("speed_to_multiplier")
 
     local speedValue = math.abs(currentSpeed)
     local multiplier = Game.GetStatsDataSystem():GetValueFromCurve(_CNAME_VEHICLE_UI, speedValue, _CNAME_SPEED_TO_MULTIPLIER)
