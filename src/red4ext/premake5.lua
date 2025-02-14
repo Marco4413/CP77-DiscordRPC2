f55a28db2ed5fbebd885e532e14e1e34830fe9a4
Marco <46133335+Marco4413@users.noreply.github.com>
2025-02-14 13:35:26 +0100

    Reindented build scripts

--- src/red4ext/premake5.lua
+++ src/red4ext/premake5.lua
@@ -1,31 +1,31 @@
 include "libs"
 
 project "cp77rpc2"
-   kind "SharedLib"
-   language "C++"
-   cppdialect "C++20"
+  kind "SharedLib"
+  language "C++"
+  cppdialect "C++20"
 
-   location "build"
-   targetdir "%{prj.location}/%{cfg.buildcfg}"
+  location "build"
+  targetdir "%{prj.location}/%{cfg.buildcfg}"
 
-   includedirs {
-      "include",
-      "libs/RED4ext.SDK/include",
-      "libs/discord_game_sdk/cpp",
-   }
-   
-   files { "src/**.cpp", "include/**.h" }
-   links "discord_game_sdk"
+  includedirs {
+    "include",
+    "libs/RED4ext.SDK/include",
+    "libs/discord_game_sdk/cpp",
+  }
+  
+  files { "src/**.cpp", "include/**.h" }
+  links "discord_game_sdk"
 
-   filter "toolset:gcc"
-      buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-Werror", }
+  filter "toolset:gcc"
+    buildoptions { "-Wall", "-Wextra", "-Wpedantic", "-Werror", }
 
-   filter "toolset:msc"
-      buildoptions { "/W4", "/WX", }
+  filter "toolset:msc"
+    buildoptions { "/W4", "/WX", }
 
-   filter "configurations:Debug"
-      defines "CP77RPC2_DEBUG"
-      symbols "On"
+  filter "configurations:Debug"
+    defines "CP77RPC2_DEBUG"
+    symbols "On"
 
-   filter "configurations:Release"
-      optimize "Speed"
+  filter "configurations:Release"
+    optimize "Speed"
