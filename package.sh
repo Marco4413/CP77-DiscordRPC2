73bc0855e47ef3e309e4515c95ad4e7ef5e8106c
Marco <46133335+Marco4413@users.noreply.github.com>
2025-09-12 16:20:03 +0200

    Use 7zip for packaging
    It's available on both Windows and Ubuntu CI by default.

--- package.sh
+++ package.sh
@@ -4,10 +4,9 @@ set -xe
 
 MOD_VERSION="$( git describe --tags --abbrev=0 --match='v*' | sed s/^v// )"
 
-WORKING_DIR="$( pwd )"
-ARTIFACT_DIR="$WORKING_DIR/artifact"
-
 BASE='./build'
+ARTIFACT_DIR="./artifact"
+
 CET_TARGET="./$BASE/bin/x64/plugins/cyber_engine_tweaks/mods/CP77RPC2"
 REDSCRIPT_TARGET="./$BASE/r6/scripts/CP77RPC2"
 RED4EXT_TARGET="./$BASE/red4ext/plugins/CP77RPC2"
@@ -46,6 +45,7 @@ cp './src/red4ext/libs/discord_game_sdk/lib/x86_64/discord_game_sdk.dll' "$RED4E
 
 # Create zip
 
-cd "$BASE"
-zip -9 -r "$ARTIFACT_DIR/DiscordRPC2-$MOD_VERSION.zip" bin r6 red4ext
-cd "$WORKING_DIR"
+7z a -mx9 -r -- "$ARTIFACT_DIR/DiscordRPC2-$MOD_VERSION.zip" \
+    "./$BASE/bin" \
+    "./$BASE/r6"  \
+    "./$BASE/red4ext"
