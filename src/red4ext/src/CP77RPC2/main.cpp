5e5da1d3ccb0aabeb295a6647185ec328ebc6136
Marco <46133335+Marco4413@users.noreply.github.com>
2026-03-10 16:47:42 +0100

    Updated RED4ext.SDK to v1.0.0

--- src/red4ext/src/CP77RPC2/main.cpp
+++ src/red4ext/src/CP77RPC2/main.cpp
@@ -2,17 +2,17 @@
 
 // #define DISCORD_CLIENT_ID 1025361016802005022
 
-RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::EMainReason aReason, const RED4ext::Sdk* aSdk)
+RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::v1::PluginHandle aHandle, RED4ext::v1::EMainReason aReason, const RED4ext::v1::Sdk* aSdk)
 {
     switch (aReason) {
-    case RED4ext::EMainReason::Load: {
+    case RED4ext::v1::EMainReason::Load: {
         RED4ext::CRTTISystem* rtti = RED4ext::CRTTISystem::Get();
         rtti->AddRegisterCallback(CP77RPC2::Bindings::RegisterTypes);
         rtti->AddPostRegisterCallback(CP77RPC2::Bindings::PostRegisterTypes);
         CP77RPC2::DiscordRPC::GetInstance()->Start();
         aSdk->logger->InfoF(aHandle, "Loaded!\n");
     } break;
-    case RED4ext::EMainReason::Unload: {
+    case RED4ext::v1::EMainReason::Unload: {
         CP77RPC2::DiscordRPC::GetInstance()->Stop();
         aSdk->logger->InfoF(aHandle, "Unloaded!\n");
     } break;
@@ -21,16 +21,16 @@ RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::
     return true;
 }
 
-RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
+RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::v1::PluginInfo* aInfo)
 {
     aInfo->name = L"DiscordRPC2";
     aInfo->author = L"Marco4413";
-    aInfo->version = RED4EXT_SEMVER(2, 9, 0);
-    aInfo->runtime = RED4EXT_RUNTIME_INDEPENDENT;
-    aInfo->sdk = RED4EXT_SDK_LATEST;
+    aInfo->version = RED4EXT_V1_SEMVER(2, 9, 1);
+    aInfo->runtime = RED4EXT_V1_RUNTIME_VERSION_INDEPENDENT;
+    aInfo->sdk = RED4EXT_V1_SDK_VERSION_CURRENT;
 }
 
 RED4EXT_C_EXPORT uint32_t RED4EXT_CALL Supports()
 {
-    return RED4EXT_API_VERSION_LATEST;
+    return RED4EXT_API_VERSION_1;
 }
